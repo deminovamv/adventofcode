28d36fe58f0d0388c80acc0ca774a152fed6994e
deminovamv <deminovamv@gmail.com>
2021-12-09 21:57:17 +0300

    solution day 9

---
+++ 2021/day_9p1.py
@@ -0,0 +1,18 @@
+
+with open("input_day9.txt", "r", encoding="utf-8") as f:
+    data = f.read()
+lines = data.split("\n")
+print(lines)
+t = 0
+
+for r in range(len(lines)):
+    for c in range(len(lines[r])):
+        v = []
+        for i, j in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+            if 0 <= i < len(lines) and 0 <= j < len(lines[r]):
+                v.append(int(lines[i][j]))
+        k = int(lines[r][c])
+        if k < min(v):
+            t += 1 + k
+
+print(t)
